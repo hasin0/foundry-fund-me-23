1ebe5922550b764b838a1c174158eef00843b993
hasin0 <hasino2258@gmail.com>
2024-10-13 00:35:22 +0100

    foundry fundme

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        // console.log(fundMe.i_owner());
+        // console.log(msg.sender);
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
